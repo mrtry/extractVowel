5d7b606903205703025cbf61b62f191eac616c00
mrtry <yamamoto9302@gmai.com>
2015-12-22 15:34:18 +0900

    サンプリング周波数によって、分割するファイルの長さを調整されるようにした

--- modules/frame.py
+++ modules/frame.py
@@ -4,24 +4,37 @@
 import wave
 import cmd
 import glob
+import time
+from progressbar import ProgressBar, Percentage, Bar, ETA
 
 def splitFrame(fileName):
     wavFile = fileName + '.wav'
     length = getFrame(wavFile)
-    loop = 1 + (int)(length / 4000)
+    frameRate = getFramerate(wavFile)
 
     x0 = 0
-    x1 = 4000
-    frameShift = 2000
+    x1 = frameRate / 4
+    frameShift = x1 / 2
+    loop = 1 + (int)(length / (frameRate / 4))
 
     cmd.wav2raw(wavFile)
     rawFile = fileName + '.raw'
+
+    widgets = ['[' + wavFile + ':splitFrame]    InProgress:', Percentage(), '   |   ', ETA()]
+    progress = ProgressBar(widgets=widgets, maxval=loop).start()
+    count = 0
+
     for i in range(0,loop):
         splitedFileName = splitRaw(fileName, x0, x1)
         x0 = x0 + frameShift
         x1 = x1 + frameShift
         cmd.raw2wav(splitedFileName)
 
+        count += 1
+        progress.update(count)
+        time.sleep(0.01)
+
+    print ""
     cmd.mkdir('splitedWav')
     beforeWavFileList = glob.glob('./splitedRaw/*.wav')
     afterWavFileList = []
@@ -34,6 +47,10 @@ def splitFrame(fileName):
 def getFrame(wavFile):
     wav = wave.open(wavFile, "r")
     return wav.getnframes()
+
+def getFramerate(wavFile):
+    wav = wave.open(wavFile, "r")
+    return wav.getframerate()
 
 def splitRaw(fileName, x0, x1):
     cmd.mkdir('splitedRaw')
